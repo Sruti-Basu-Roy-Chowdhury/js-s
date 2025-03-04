a354c1acbd7e43c42d99cf484b9167aa693f2f71
Sruti Basu Roy Chowdhury <163338727+Sruti-Basu-Roy-Chowdhury@users.noreply.github.com>
2025-03-04 20:12:24 +0530

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+ "use strict"; //treat all Js code as newer version
+
+
+// alert(3+3) //we are using node js not browser
+
+
+let name = "Sruti"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+//Number => 2 to the power 53
+//bigint
+//string=>""
+//boolean=>true /fontVariantAlternates: 
+//null => standalone value
+//undefined =>
+//symbol=> unique
+
+
+//object
+
+console.log(typeof"Sruti");
+console.log(typeof null);
+console.log(typeof undefined);
